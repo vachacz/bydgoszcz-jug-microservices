071760b5a7d95a4a86c2c1ab2851a2675fe7add5
vachacz <vachacz@gmail.com>
2016-11-09 22:03:19 +0100

    sync communication between jugs

--- bydgoszcz-jug/src/main/java/pl/jug/bydgoszcz/BydgoszczJugApplication.java
+++ bydgoszcz-jug/src/main/java/pl/jug/bydgoszcz/BydgoszczJugApplication.java
@@ -1,11 +1,23 @@
 package pl.jug.bydgoszcz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @EnableEurekaClient
 @EnableFeignClients
@@ -17,4 +29,40 @@ public class BydgoszczJugApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BydgoszczJugApplication.class, args);
 	}
+
+	private static final Logger logger = LoggerFactory.getLogger(BydgoszczJugApplication.class);
+
+	@org.springframework.web.bind.annotation.RestController
+	public class RestController {
+
+		@Autowired
+		private TorunJugClient torunJugClient;
+
+		@RequestMapping(path = "/inviteTorunJug", method = RequestMethod.GET)
+		public String inviteTorunJug() {
+			logger.info("InviteTorunJug executed");
+			torunJugClient.invite();
+			return "ack";
+		}
+
+		@RequestMapping(path = "/ack", method = RequestMethod.GET)
+		public String greet() {
+			logger.info("Ack executed");
+			torunJugClient.seeyou();
+			return "ack";
+		}
+
+	}
+
+	@FeignClient("torun-jug")
+	public interface TorunJugClient {
+
+		@RequestMapping(value = "/invite")
+		void invite();
+
+		@RequestMapping(value = "/seeyou")
+		void seeyou();
+
+	}
+
 }
--- torun-jug/src/main/java/pl/jug/torun/TorunJugApplication.java
+++ torun-jug/src/main/java/pl/jug/torun/TorunJugApplication.java
@@ -3,14 +3,25 @@ package pl.jug.torun;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
 @EnableFeignClients
@@ -28,17 +39,29 @@ public class TorunJugApplication {
 	@org.springframework.web.bind.annotation.RestController
 	public class RestController {
 
-		@RequestMapping(path = "/greet", method = RequestMethod.GET)
-		public String getGreet() {
-			logger.info("Greet executed");
+		@Autowired
+		private BydgoszczJugClient bydgoszczJugClient;
+
+		@RequestMapping(path = "/invite", method = RequestMethod.GET)
+		public String greet() {
+			logger.info("Invite executed");
+			bydgoszczJugClient.ack();
 			return "greet";
 		}
 
-		@RequestMapping(path = "/super", method = RequestMethod.GET)
-		public String getSuper() {
-			logger.info("Super executed");
-			return "super";
+		@RequestMapping(path = "/seeyou", method = RequestMethod.GET)
+		public String seeyou() {
+			logger.info("Seeyou executed");
+			return "seeyou";
 		}
+
+	}
+
+	@FeignClient("bydgoszcz-jug")
+	public interface BydgoszczJugClient {
+
+		@RequestMapping(value = "/ack")
+		void ack();
 
 	}
 
